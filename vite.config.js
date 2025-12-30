5524ba8bbf1345d450282b8e1d82bf1b8527eb09
ohadb123 <ohadb123@gmail.com>
2025-12-30 21:03:25 +0200

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    sourcemap: true,
+  },
+});
